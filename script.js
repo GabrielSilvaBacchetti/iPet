f5ff773e442f3c639c6a51889bf1789e47a9f310
Gabriel Silva <37548272+GaelBSilva@users.noreply.github.com>
2018-09-19 19:56:03 -0300

    Add files via upload
    apenas um projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+ funtion envioDados(){
+ 	var name = parseInt(document.getElementById('name').value);
+ 	var sobrenome = parseInt(document.getElementById('sobrenome').value);
+ 	var dia = parseInt(document.getElementById('dia').value);
+ 	var mês = parseInt(document.getElementById('mês').value);
+ 	var ano = parseInt(document.getElementById('ano').value);
+ 	var senha = parseInt(document.getElementById('senha').value);
+ 	var email = parseInt(document.getElementById('email').value);
+ 	var cidade = parseInt(document.getElementById('cidade').value);
+ 	var estado = parseInt(document.getElementById('estado').value);
+ 	var CRMV = parseInt(document.getElementById('CRMV').value);
+ 	var nomeAnimal = parseInt(document.getElementById('nomeAnimal').value);
+ 	var raça = parseInt(document.getElementById('raça').value);
+
+ 	alert('oi');
+ }
